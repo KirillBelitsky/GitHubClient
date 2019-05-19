c2e429b5fbb3be76cfd775293149f5b53b5f6615
KirillBelitsky <kirill_belitsky@mail.ru>
2019-05-20 00:58:33 +0300

    edit login activity

--- app/src/main/java/com/example/githubclient/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/githubclient/ui/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.githubclient.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -10,6 +12,9 @@ import com.example.githubclient.R;
 import com.example.githubclient.network.service.NetworkService;
 import com.example.githubclient.model.User;
 import com.example.githubclient.network.session.UserSession;
+import com.unstoppable.submitbuttonview.SubmitButton;
+
+import org.apache.commons.lang3.StringUtils;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,7 +26,10 @@ public class LoginActivity extends AppCompatActivity {
     private static final String PASSWORD_REGEX = "^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,}\\$";
 
     private TextInputEditText mUsername;
+    private TextInputLayout mUsernameLayout;
     private TextInputEditText mPassword;
+    private TextInputLayout mPasswordLayout;
+    private SubmitButton loginButton;
     private UserSession userSession;
     private User user;
 
@@ -30,39 +38,86 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mUsername = (TextInputEditText) findViewById(R.id.user_name_et);
-        mPassword = (TextInputEditText) findViewById(R.id.password_et);
+        loginButton = findViewById(R.id.login_bn);
+        mUsername = findViewById(R.id.user_name_et);
+        mPassword = findViewById(R.id.password_et);
+        mUsernameLayout = findViewById(R.id.user_name_layout);
+        mPasswordLayout = findViewById(R.id.password_layout);
+
         userSession = new UserSession(getApplicationContext());
+
     }
 
 
     public void onLoginClick(View view) {
 
-        final String token = userSession.createToken(mUsername.getText().toString(), mPassword.getText().toString());
+        if(loginCheck()) {
+            final String token = userSession.createToken(mUsername.getText().toString(), mPassword.getText().toString());
+            loginButton.setEnabled(false);
 
-        NetworkService.getInstance().
-                getUserApi().
-                login(token).
-                enqueue(new Callback<User>() {
-                    @Override
-                    public void onResponse(Call<User> call, Response<User> response) {
-                        user = response.body();
+            NetworkService.getInstance().
+                    getUserApi().
+                    login(token).
+                    enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            user = response.body();
 
-                        if (user != null) {
-                            userSession.saveCredentials(user, token);
-                            loginSuccesfull();
+                            if (user != null) {
+                                userSession.saveCredentials(user, token);
+
+                                loginButton.doResult(true);
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                    }
+                                }, 2500);
+                                loginSuccesfull();
+                            }
+
+                            loginButton.doResult(false);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    loginButton.reset();
+                                    loginButton.setEnabled(true);
+                                }
+                            }, 1000);
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<User> call, Throwable t) {
-
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                        }
+                    });
+        }
     }
 
     private void loginSuccesfull() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
+
+    private boolean loginCheck(){
+        boolean valid = true;
+        String userName = mUsername.getText().toString();
+        String password = mPassword.getText().toString();
+        if(StringUtils.isBlank(userName)){
+            valid = false;
+            mUsernameLayout.setError(getString(R.string.username_invalid));
+
+        }else{
+            mUsernameLayout.setErrorEnabled(false);
+        }
+        if(StringUtils.isBlank(password)){
+            valid = false;
+            mPasswordLayout.setError(getString(R.string.password_invalid));
+        }else{
+            mPasswordLayout.setErrorEnabled(false);
+        }
+        if(!valid)
+            loginButton.reset();
+
+        return valid;
+    }
+
 }
