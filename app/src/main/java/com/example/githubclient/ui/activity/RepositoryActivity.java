1650b52b61ed328f527c90f62fdf046ccad2a58f
KirillBelitsky <kirill_belitsky@mail.ru>
2019-05-29 22:16:06 +0300

    start add repository

---
+++ app/src/main/java/com/example/githubclient/ui/activity/RepositoryActivity.java
@@ -0,0 +1,21 @@
+package com.example.githubclient.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.githubclient.R;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
